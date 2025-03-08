d7f1c02d69ade45d969def5315404afbae1b8e87
0xyosef <yosef2020go@gmail.com>
2025-03-08 23:43:32 +0200

    add : add presence plugin

---
+++ config/plugins/editor/presence.nix
@@ -0,0 +1,15 @@
+_: {
+
+  plugins = {
+    presence-nvim.enable = true;
+    showTime = true;
+    neovimImageText = "NVIM";
+    editingText = "Editing %s";
+    fileExplorerText = "Browsing %s";
+    gitCommitText = "Committing changes";
+    pluginManagerText = "Managing plugins";
+    readingText = "Reading %s";
+    workspaceText = "Working on %s";
+    lineNumberText = "Line %s out of %s";
+  };
+}
